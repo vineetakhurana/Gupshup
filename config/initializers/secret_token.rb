9880d4b6f2fcc8dbaadd9ea7c21037872059b7f2
mihirvj <mihir.joshi@outlook.com>
2013-09-09 15:11:31 -0400

    Blank rails application.. We will use it hence forth for backchannel app development

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gupshup::Application.config.secret_key_base = '8c71dd4f78c479d72e5bacb42ece36d548c5508c72a6960791ef358e113b100566de24afbf89ea8b0696a7d1fae8af2ec50d108c388269d3b6f28fd11014e360'
